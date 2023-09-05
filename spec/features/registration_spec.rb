2a2f119b208395af3df81a7f2402a3ee08ec1081
Kaina Cockett <kainacockett@gmail.com>
2023-09-05 13:35:19 -0600

    test: create Sad Path tests

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "User Registration" do
   it 'can create a user with a name and unique email' do
+    # ***Authentication Challenge, US1 ***
+    # As a visitor 
+    # When I visit `/register`
+    # I see a form to fill in my name, email, password, and password confirmation.
+    # When I fill in that form with my name, email, and matching passwords,
+    # I'm taken to my dashboard page `/users/:id`
+
     visit register_path
 
     fill_in :user_name, with: 'User One'
@@ -27,5 +34,46 @@ RSpec.describe "User Registration" do
 
     expect(current_path).to eq(register_path)
     expect(page).to have_content("Email has already been taken")
+  end
+
+  it "SAD PATH - missing name" do
+    # ***Authentication Challenge, US1 ***
+    # As a visitor 
+    # When I visit `/register`
+    # and I fail to fill in my name, unique email, OR matching passwords,
+    # I'm taken back to the `/register` page
+    # and a flash message pops up, telling me what went wrong
+
+    visit register_path
+    fill_in :user_email, with:'user1@example.com'
+    fill_in :user_password, with:'Password'
+    fill_in :user_password_confirmation, with:'Password'
+    click_button 'Create New User'
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Validation failed:  Missing Name")
+  end
+
+  it "SAD PATH - missing email" do
+    visit register_path
+    fill_in :user_name, with: 'User One'
+    fill_in :user_password, with:'Password'
+    fill_in :user_password_confirmation, with:'Password'
+    click_button 'Create New User'
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Validation failed:  Missing Email")
+  end
+
+  it "SAD PATH - non-matching passwords" do
+    visit register_path
+    fill_in :user_name, with: 'User One'
+    fill_in :user_email, with:'user1@example.com'
+    fill_in :user_password, with:'Password'
+    fill_in :user_password_confirmation, with:'NotPassword'
+    click_button 'Create New User'
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Validation failed:  Password verification failed")
   end
 end
