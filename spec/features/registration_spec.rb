2b84bca3ef9615dac3b047ec05b84cc781d8f56d
Kaina Cockett <kainacockett@gmail.com>
2023-09-05 13:24:24 -0600

    fix: registration_spec and user create

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -6,35 +6,26 @@ RSpec.describe "User Registration" do
 
     fill_in :user_name, with: 'User One'
     fill_in :user_email, with:'user1@example.com'
+    fill_in :user_password, with:'Password'
+    fill_in :user_password_confirmation, with:'Password'
     click_button 'Create New User'
-
+    
     expect(current_path).to eq(user_path(User.last.id))
     expect(page).to have_content("User One's Dashboard")
   end 
 
   it 'does not create a user if email isnt unique' do 
-    User.create(name: 'User One', email: 'notunique@example.com')
+    User.create(name: 'User One', email: 'notunique@example.com', password: "password", password_confirmation: "password")
 
     visit register_path
     
     fill_in :user_name, with: 'User Two'
     fill_in :user_email, with:'notunique@example.com'
+    fill_in :user_password, with:'Password'
+    fill_in :user_password_confirmation, with:'Password'
     click_button 'Create New User'
 
     expect(current_path).to eq(register_path)
     expect(page).to have_content("Email has already been taken")
-  end
-
-  describe "*Authentication Challenge*" do
-    describe "As a visitor, When I visit '/register'" do
-      it "I see a form to fill in my name, email, password, and password confirmation." do
-        visit register_path
-
-        expect(page).to have_css("#registration_form")
-        within "#registration_form" do
-          #
-        end
-      end
-    end
   end
 end
