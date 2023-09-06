7570857f06d283a08c87dc8949d26518cfa8cd60
Kaina Cockett <kainacockett@gmail.com>
2023-09-06 14:51:22 -0600

    test: verify auth needed to create viewing party

--- spec/features/movie_show_spec.rb
+++ spec/features/movie_show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Movies Index Page' do
+RSpec.describe 'Movies Show Page' do
   before do 
     @user1 = User.create(name: "User One", email: "user1@test.com", password: "password", password_confirmation: "password")
     i = 1
@@ -34,4 +34,15 @@ RSpec.describe 'Movies Index Page' do
     expect(page).to have_content(movie_1.description)
     expect(page).to have_content(movie_1.rating)
   end 
+
+  describe "*** Authentication Challenge2 Pt2 ***" do
+    it "US4 As a visitor, If I go to a movies show page, And click the button to create a viewing party, I'm redirected to the movies show page, and a message appears to let me know I must be logged in or registered to create a movie party." do
+      visit movie_path(User.first, Movie.first)
+      save_and_open_page
+      click_button "Create Viewing Party"
+
+      expect(current_path).to eq movie_path(User.first, Movie.first)
+      expect(page).to have_content("You must be logged in or registered to create a movie party.")
+    end
+  end
 end
