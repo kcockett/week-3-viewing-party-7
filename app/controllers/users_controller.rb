89ce83836332a4d7e683912cf0ab6e62385d0c93
Kaina Cockett <kainacockett@gmail.com>
2023-09-06 10:33:56 -0600

    test: landing page links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController <ApplicationController
   def create 
     user = User.create(user_params)
     if user.save
+      session[:user_id] = user.id
       redirect_to user_path(user)
     else  
       flash[:error] = user.errors.full_messages.to_sentence
